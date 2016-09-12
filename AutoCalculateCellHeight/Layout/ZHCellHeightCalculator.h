07348d706d3daa885bd6724c6de7f4659e5374e3
Mr.jiang <414816566@qq.com>
2016-09-13 00:31:20 +0800

    Modify copyright information

--- AutoCalculateCellHeight/Layout/ZHCellHeightCalculator.h
+++ AutoCalculateCellHeight/Layout/ZHCellHeightCalculator.h
@@ -1,6 +1,9 @@
 //
-//  ZHCellHeightCalculator.h
-//  AutoCalculateCellHeight
+//  GitHub:https://github.com/zhuozhuo
+
+//  博客：http://www.jianshu.com/users/39fb9b0b93d3/latest_articles
+
+//  欢迎投稿分享：http://www.jianshu.com/collection/4cd59f940b02
 //
 //  Created by yongzhuoJiang on 16/9/12.
 //  Copyright © 2016年 zhuo. All rights reserved.
